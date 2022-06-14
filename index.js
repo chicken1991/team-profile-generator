3575200f92dad72e47abe1f1d822b7034fb83021
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-06-14 07:35:37 -0700

    some inquirer building. Todo: fix line 70

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // Dependancies
-const inquierer = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 
 // lib files
 const manager = require('./lib/Manager');
 const engineer = require('./lib/Engineer');
 const intern = require('./lib/Intern');
+const { exit } = require('process');
 
 //TODO: Create an object that utilizes the manager, engineer, and intern classes to append appropriate object properties based
 // on the user input
+let manObj;
 let empObj; 
 
 const promptManager = () => {
-    return inquirer.prompt(
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -33,7 +35,23 @@ const promptManager = () => {
             name: 'officeNumber',
             message: "Manager's office number: ",
           }
-    )};
+        ]);
+};
+
+const promptProceed = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'answer',
+            message: "Would you like to add an Engineer, Intern, or exit? ",
+            choises: [
+                "Engineer",
+                "Intenrn",
+                "Exit"
+            ]
+          }
+        ]);
+};
 
 // input function
 // Demand to fill out manager - cannot continue without manager
@@ -41,16 +59,30 @@ const promptManager = () => {
 // IF engineer, proceed with adding new Engineer
 // IF intern, proceed with adding new Intern
 // after each selection, add option to add Engineer or Intern OR Finish
-const init = () => {
+function init() {
     //prompts for manager info
-    promptManager();
+    promptManager()
+        .then((answers) => console.log(answers))
+        .catch((err) => console.error(err));
 
+        //TODO: FIX THIS, IF STATEMENT INSIDE OF A .THEN???
+    promptProceed()
+    .then((answer) =>
+        if (answer === "Engineer"){
+            promptEngineer();
+        } else if (answer === "Intenrn") {
+            promptIntern();
+        } else {
+            console.log("Exiting and writing to file")
+        };
+    )
+    .catch((err) => console.error(err));
     //prompt to add intern, engineer, or Finish
-    promptEmployee();
+    // promptEmployee();
 }
 
 //create HTML and insert appropriate ${data.stuff} for each modal
-const renderHTML = (data) => {}
+const renderHTML = (data) => {};
 
 init();
 renderHTML();
