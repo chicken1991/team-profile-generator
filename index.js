8c4760fc450d262c4bcbcde94b9149dd62a8f18e
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-06-14 21:44:07 -0700

    some changes - still not functional

--- index.js
+++ index.js
@@ -6,51 +6,51 @@ const fs = require('fs');
 const manager = require('./lib/Manager');
 const engineer = require('./lib/Engineer');
 const intern = require('./lib/Intern');
-const { exit } = require('process');
+// const { exit } = require('process'); //wtf is this? I don't remember this
 
 //TODO: Create an object that utilizes the manager, engineer, and intern classes to append appropriate object properties based
 // on the user input
 let manObj;
-let empObj; 
+let empObj;
 
-const promptManager = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "Manager's name: ",
-          },
-          {
-            type: 'input',
-            name: 'id',
-            message: "Manager's Employee ID",
-          },
-          {
-            type: 'input',
-            name: 'email',
-            message: "Manager's email address: ",
-          },
-          {
-            type: 'input',
-            name: 'officeNumber',
-            message: "Manager's office number: ",
-          }
-        ]);
+async function promptManager(){
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "Manager's name: ",
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Manager's Employee ID",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Manager's email address: ",
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "Manager's office number: ",
+    }
+  ]);
 };
 
-const promptProceed = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'answer',
-            message: "Would you like to add an Engineer, Intern, or exit? ",
-            choises: [
-                "Engineer",
-                "Intenrn",
-                "Exit"
-            ]
-          }
-        ]);
+async function promptProceed() {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'answer',
+      message: "Would you like to add an Engineer, Intern, or exit? ",
+      choices: [
+        "Engineer",
+        "Intenrn",
+        "Exit"
+      ]
+    }
+  ]);
 };
 
 // input function
@@ -60,29 +60,28 @@ const promptProceed = () => {
 // IF intern, proceed with adding new Intern
 // after each selection, add option to add Engineer or Intern OR Finish
 function init() {
-    //prompts for manager info
-    promptManager()
-        .then((answers) => console.log(answers))
-        .catch((err) => console.error(err));
-
-        //TODO: FIX THIS, IF STATEMENT INSIDE OF A .THEN???
-    promptProceed()
-    .then((answer) =>
-        if (answer === "Engineer"){
-            promptEngineer();
-        } else if (answer === "Intenrn") {
-            promptIntern();
-        } else {
-            console.log("Exiting and writing to file")
-        };
-    )
+  //prompts for manager info
+  promptManager()
+    .then((answers) => console.log(answers)) // console log for now TODO: write this to an object which can be called, then written to html
     .catch((err) => console.error(err));
-    //prompt to add intern, engineer, or Finish
-    // promptEmployee();
+
+  promptProceed()
+    .then((answer) => {
+      if (answer === "Engineer") {
+        promptEngineer();
+      } else if (answer === "Intenrn") {
+        promptIntern();
+      } else {
+        console.log("Exiting and writing to file")
+      };
+    })
+    .catch((err) => console.error(err));
+  //prompt to add intern, engineer, or Finish
+  // promptEmployee();
 }
 
 //create HTML and insert appropriate ${data.stuff} for each modal
-const renderHTML = (data) => {};
+const renderHTML = (data) => { };
 
 init();
 renderHTML();
